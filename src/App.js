ae1f32b477be0084366c664e31799f6d1a53a2c6
Viet Bui <buiminhviet.2002@gmail.com>
2021-04-16 20:57:35 -0500

    Added input and search box

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import './App.css';
 import Stock from './stocks'
 
+function getInputValue(){
+  let inputCode = document.getElementById("inputData").value;
+  alert(inputCode);
+}
+
 function App() {
   return (
     <div className="App">
